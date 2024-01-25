f3308e5fdb6f782465a4f8899f24d7d3789d2e7f
ajm van der ende <adr1200c.me.com>
2024-01-25 12:47:43 +0100

    Initial Commit

---
+++ SceneView/SceneViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneViewApp.swift
+//  SceneView
+//
+//  Created by fs73tg on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SceneViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
